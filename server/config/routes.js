376b91045f5bac557748ca6f8bd0d335276f1d5b
Chyld Medford <chyld.medford@gmail.com>
2015-02-17 10:01:52 -0800

    bootstrap

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,5 +2,7 @@
 
 module.exports = [
   {method: 'get', path: '/{param*}', config: require('../routes/general/static')},
-  {method: 'get', path: '/', config: require('../routes/general/home')}
+  {method: 'get', path: '/', config: require('../routes/general/home')},
+  {method: 'get', path: '/faq', config: require('../routes/general/faq')},
+  {method: 'get', path: '/contact', config: require('../routes/general/contact')}
 ];
--- server/config/views.js
+++ server/config/views.js
@@ -3,5 +3,6 @@
 module.exports = {
   engines: {jade: require('jade')},
   path: __dirname + '/../views',
+  helpersPath: __dirname + '/../views/helpers',
   compileOptions: {pretty: true, doctype: 'html'}
 };
