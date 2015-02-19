ccdb331b3ad2ce36ce46e51169fded16506ec07f
Tommy Janszen <tommy.janszen@gmail.com>
2015-02-18 17:36:28 -0800

    Add css tweeks

--- server/routes/animals/index.js
+++ server/routes/animals/index.js
@@ -6,26 +6,26 @@ var _ = require('lodash');
 
 module.exports = {
   handler: function(request, reply) {
+    var q = request.query;
 
-  var q = request.query;
+    q = _.pick(q, function(v){
+      return v;
+    });
 
-  q = _.pick(q, function(v) {
-    return v;
-  })
-   if(q.min) {
-     q.age = {$gte : q.min};
-     delete q.min;
-   }
+    if(q.min){
+      q.age = {$gte : q.min};
+      delete q.min;
+    }
 
-   if(q.max) {
-     q.age = q.age || {};
-     _.merge(q.age, {$lte : q.max});
-     delete q.max;
-   }
+    if(q.max){
+      q.age = q.age || {};
+      _.merge(q.age, {$lte : q.max});
+      delete q.max;
+    }
 
-  q.isAdopted = false;
+    q.isAdopted = false;
 
-    Animal.find(request.query, function(err, animals) {
+    Animal.find(q, function(err, animals) {
       reply.view('templates/animals/index', {path:'/animals', active:active, animals:animals});
     });
   }
--- server/routes/clients/show.js
+++ server/routes/clients/show.js
@@ -5,7 +5,7 @@ var active = require('../../views/helpers/active');
 
 module.exports = {
   handler: function(request, reply) {
-    Client.findOne({_id:request.params.clientId},function(err, client) {
+    Client.findOne({_id:request.params.clientId}).populate('pets').exec(function(err, client) {
       reply.view('templates/clients/show', {path:'/clients', active:active, client:client});
     });
   }
